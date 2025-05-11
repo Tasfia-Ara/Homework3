08ea04f8217647fab4afec4befb0367473a95591
tasfia_ara <tasfia_ara@outlook.com>
2025-05-11 15:15:43 -0400

    Reduce.java fixed, unit test 3 should run

--- hw1-git-activity/src/Reduce.java
+++ hw1-git-activity/src/Reduce.java
@@ -2,11 +2,14 @@ public class Reduce {
     public static int main(int n) {
         int count = 0;
         while(n > 0){
-            if (!(n % 2 == 0)) {
-                n = n -1;
+            if (n % 2 != 0) {
+                n = n - 1;
+                count++;
             }
-            n = n/2;
-            count++;
+            else {
+                n = n/2;
+                count++;
+            }
         }
         return count;
     }
