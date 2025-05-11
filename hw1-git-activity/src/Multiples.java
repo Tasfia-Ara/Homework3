1b59341d26af06efe45d491ea3333b27f7515f25
tasfia_ara <tasfia_ara@outlook.com>
2025-05-11 12:59:23 -0400

    one off error

--- hw1-git-activity/src/Multiples.java
+++ hw1-git-activity/src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static int main(int n, int a, int b) {
         int count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
                 count++;
             }
@@ -9,9 +9,9 @@ public class Multiples {
         return count;
 //        System.out.println(count);
     }
-//    public static int main(){
-//        return Multiples.main(nulll, 1000, 3, 5)
-//    }
+    public static int main(){
+        return Multiples.main(1000, 3, 5)
+    }
 //    Multiples obj = new Multiples(100);
 }
 
