02205483ee273b759cfed5a34af6e258e9aa1b24
tasfia_ara <tasfia_ara@outlook.com>
2025-05-11 14:25:46 -0400

    fix

--- hw1-git-activity/src/Reduce.java
+++ hw1-git-activity/src/Reduce.java
@@ -1,20 +1,16 @@
 public class Reduce {
-    public static void main(String[] args, int n) {
-        reduceMethod(n);
-    }
-
-    private static int reduceMethod(int n) {
+    public static int main(int n) {
         int count = 0;
         while(n > 0){
             if (!(n % 2 == 0)) {
                 n = n -1;
             }
-            n = n /2;
+            n = int(n/2);
             count++;
         }
         return count;
-//        System.out.println(count);
     }
+
 }
 
 
