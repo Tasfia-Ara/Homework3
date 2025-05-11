65457a3a7591783f13a9c7c33dc445c01e4a87cb
tasfia_ara <tasfia_ara@outlook.com>
2025-05-11 12:52:56 -0400

    main method fixed T1

--- hw1-git-activity/src/Multiples.java
+++ hw1-git-activity/src/Multiples.java
@@ -1,5 +1,5 @@
 public class Multiples {
-    public static int main(String[] args, int n, int a, int b) {
+    public static int main(int n, int a, int b) {
         int count = 0;
         for (int i = 0; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
@@ -9,6 +9,9 @@ public class Multiples {
         return count;
 //        System.out.println(count);
     }
+//    public static int main(){
+//        return Multiples.main(nulll, 1000, 3, 5)
+//    }
 //    Multiples obj = new Multiples(100);
 }
 
