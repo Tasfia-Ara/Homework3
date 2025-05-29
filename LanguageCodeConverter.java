6e5308511ed7096cca06f46a444c2ab24d322ef3
boyleoli <olivia.boyle@mail.utoronto.ca>
2025-05-29 19:09:34 -0400

    Add files via upload
    language code converter (fixed checkstyle errors)

--- LanguageCodeConverter.java
+++ LanguageCodeConverter.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * This class provides the service of converting language codes to their names.
